7201177834a15f2ec69a02cb245a3315942aee9c
didi <176219362+didi-vgn@users.noreply.github.com>
2025-05-05 18:58:10 +0300

    Fix for GH Pages 404 error.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "copy-index-to-404",
+      closeBundle: () => {
+        const distPath = path.resolve(__dirname, "dist");
+        fs.copyFileSync(`${distPath}/index.html`, `${distPath}/404.html`);
+      },
+    },
+  ],
   base: "/minesweeper-react",
 });
