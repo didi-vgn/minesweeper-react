e76362b3249c4bb119eca3c0fb3debdab125ec71
didi <176219362+didi-vgn@users.noreply.github.com>
2025-05-06 14:17:21 +0300

    Fixed end of game screen bug.

--- client/src/game/components/GameOverScreen.jsx
+++ client/src/game/components/GameOverScreen.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-const BASE_PATH = import.meta.env.BASE_URL;
 
 export default function GameOverScreen() {
   const [frameIndex, setFrameIndex] = useState(0);
@@ -18,7 +17,7 @@ export default function GameOverScreen() {
     <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center bg-[rgba(0,0,0,0.5)] z-30'>
       <div className='overflow-hidden w-[512px] h-[512px]'>
         <div
-          className={`w-[1536px] h-[512px] bg-[url(${BASE_PATH}/other/game_over.png)] mt-15`}
+          className='w-[1536px] h-[512px] bg-[url(/minesweeper-react/other/game_over.png)] mt-15'
           style={{ transform: `translateX(-${frameIndex}px)` }}
         ></div>
       </div>
